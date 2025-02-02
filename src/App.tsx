f9f700c50845efd14b2818ca8a14ceab26787a80
Haocheng Su <haocsu@utu.fi>
2025-02-02 14:36:12 +0200

    Refactor App component structure for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { Button, Divider, Container, Typography } from '@mui/material';
+import { BrowserRouter as Router} from "react-router-dom";
+import { Container} from '@mui/material';
 
-import Home from './components/home';
+import Home from './components/Home';
 
 function App() {
-
   return (
-    <Router>
-      <div className='app'>
-      
-      <Container 
-        maxWidth="xl" // Adjust the max width (xs, sm, md, lg, xl)
-        disableGutters
-        sx={{
-          marginTop: 10, // Add margin at the top
-          marginBottom: 10, // Add margin at the bottom
-          paddingLeft: 10,
-          paddingRight: 10,
-        }}
-      >
-      <Home />
-
-
-      </Container>
-      </div>
-    </Router>
-  )
+      <Router>
+        <div className='app'>
+          <Container 
+            maxWidth="xl"
+            disableGutters
+            sx={{
+              marginTop: 10,
+              marginBottom: 10,
+              paddingLeft: 10,
+              paddingRight: 10,
+            }}
+          >
+            <Home />
+          </Container>
+        </div>
+      </Router>
+  );
 }
 
 export default App
